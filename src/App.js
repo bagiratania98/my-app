482566235167e7e01433ab79b720dbf09c22be57
Tetiana Bahyra <bagiratana@gmail.com>
2022-06-30 10:44:26 +0300

    add name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tetiana Bahura
         </a>
       </header>
     </div>
